52b28d1f8195f0531ba619174c3525c7b5c9e0ae
Sahaja-tech <156084098+Sahaja-tech@users.noreply.github.com>
2024-12-02 16:22:50 +0530

    Update challenge_two.rb

--- challenge_two.rb
+++ challenge_two.rb
@@ -1,9 +1,4 @@
-def reverse_array(arr)
-  arr.reverse
-end
-
-input = [1, 2, 3]
-output = reverse_array(input)
+input = [1,2,3]
+output = input.reverse()
 puts input.inspect
-puts output.inspect 
-#Expected Output: [3, 2, 1]
+puts output.inspect
