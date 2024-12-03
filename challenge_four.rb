e6dc5f6db720d1c984bf077688a9d0523d2bcf3c
Sahaja-tech <156084098+Sahaja-tech@users.noreply.github.com>
2024-12-03 09:08:52 +0530

    Create challenge_four.rb

---
+++ challenge_four.rb
@@ -0,0 +1,13 @@
+#Hash: ruby method to sort a hash based on key Input: {"a": 2, "c": 2, "b": 3} ourput: "a": 2, "b": 3, "c": 2 }
+def sort_hash_key(hash)
+  unless hash.is_a?(Hash)
+  end
+
+  # Sort
+  hash.sort.to_h
+end
+
+input_hash = { "a": 2, "c": 2, "b": 3 }
+sorted_hash = sort_hash_key(input_hash) 
+
+puts sorted_hash #expected output: {:a=>2, :b=>3, :c=>2}
