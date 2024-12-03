09154d08252540f5824c38953323506869ad9565
Sahaja-tech <156084098+Sahaja-tech@users.noreply.github.com>
2024-12-03 16:24:06 +0530

    Create challenge_seven.rb

---
+++ challenge_seven.rb
@@ -0,0 +1,18 @@
+def concat_strings(string_array)
+  string_array.join
+end
+
+# Array for input lines
+lines = []
+
+puts "Enter lines of text (type 'END' to finish):"
+
+# Read multiple lines of input until the user types 'END'
+while true
+  line = gets.chomp
+  break if line == 'END'  # Exit the loop if 'END' is typed
+  lines << line  # Add the line to the array
+end
+
+result = concat_strings(lines)
+puts result  # Output the concatenated string
